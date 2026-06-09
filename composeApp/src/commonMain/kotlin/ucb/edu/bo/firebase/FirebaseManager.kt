bf06932ac8bc21d35a6dc8b2b6240e122e81202a
isabelhidalgo-source <isabel.hidalgo@ucb.edu.bo>
2026-06-08 20:45:41 -0400

    fix: Delete task

--- composeApp/src/commonMain/kotlin/ucb/edu/bo/firebase/FirebaseManager.kt
+++ composeApp/src/commonMain/kotlin/ucb/edu/bo/firebase/FirebaseManager.kt
@@ -3,8 +3,7 @@ package ucb.edu.bo.firebase
 import ucb.edu.bo.todoApp.task.domain.model.TaskModel
 
 /**
- * MODIFICADO: Se declaran los 3 nuevos métodos para la sincronización delta.
- * Actualiza tu FirebaseManager.kt (commonMain) con este contenido.
+ * MODIFICADO: Se declaran los nuevos métodos para la sincronización delta y borrado.
  */
 expect class FirebaseManager() {
     suspend fun saveData(path: String, value: String)
@@ -13,4 +12,7 @@ expect class FirebaseManager() {
     suspend fun uploadTask(task: TaskModel, hash: String)
     suspend fun fetchRemoteHashes(): Map<Int, String>
     suspend fun fetchRemoteTasks(): List<TaskModel>
+    
+    // NUEVO: Borrado remoto
+    suspend fun deleteTask(taskId: Int)
 }
